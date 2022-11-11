e7126f6514c570e4265a95d1d80d4f19d0bdaafa
jhlee <jhlee@sorin.co.kr>
2022-11-11 14:10:11 +0900

    17 부모 컴포넌트로 데이터 보내기 (Emit)

---
+++ test1/src/components/InputField.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <label for="">Name</label>
+        <!-- <input type="text" v-model="name" style="padding: 30px; border: 2px solid green;"> -->
+        <!-- <input 
+            type="text" 
+            :value="name" 
+            style="padding: 30px; border: 2px solid green;"
+            @input="$emit('update-name', $event)"
+        > -->
+        <input 
+            type="text" 
+            :value="value" 
+            style="padding: 30px; border: 2px solid green;"
+            @input="$emit('input', $event.target.value)"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        // name: {
+        //     type: String,
+        //     required: true
+        // }
+        value: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        // updateName(e) {
+        //     console.log(e.target.value);
+        //     this.$emit('update-name', e.target.value); // (이벤트명, 값) 부모로 값 올리기
+        // }
+    }
+}
+</script>
--- test1/src/views/HomeView.vue
+++ test1/src/views/HomeView.vue
@@ -1,24 +1,31 @@
 <template> <!-- html code -->
   <div>
     <h1>This is Home page</h1>
-    <KossieCoder title="home title" name="Home name"></KossieCoder>
+    <form action="">
+      <!-- <InputField :name="name" @update-name="name = $event.target.value" /> -->
+      <InputField v-model="name" /> <!-- v-model은 value로 양방향 binding해주는 옵션 >> value로 props, emit하는 방법 -->
+      <button>Submit</button>
+    </form>
+    실시간으로 자식 컴포넌트에서 <br>값을 올려받는 부모의 name : <br>{{ name }}
   </div>
 </template>
 
 <script>
-import KossieCoder from '@/components/KossieCoder.vue';
+import InputField from '@/components/InputField.vue';
 export default { // data, methods, computed ....
-  components: { // 컴포넌트 등록
-    KossieCoder
+  components: {
+    InputField
   },
   data() {
     return {
-      name: 'Kossie Coder'
+      name: ''
     }
   },
-  methods: {
-
-  }
+  // methods: {
+  //   updateName(name) {
+  //     this.name = name; // 자식에서 올려받은 것을 넣어줌
+  //   }
+  // }
 }
 </script>
 
