63723eb53a4252ae6cf6e9a92333d29cf3b0728d
jhlee <jhlee@sorin.co.kr>
2022-11-11 15:36:53 +0900

    19 뷰 인스턴스 라이프사이클

--- test1/src/views/HomeView.vue
+++ test1/src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <template> <!-- html code -->
   <div>
     <h1>This is Home page</h1>
-    <form action="">
-      <!-- <InputField :name="name" @update-name="name = $event.target.value" /> -->
-      <InputField v-model="name" /> <!-- v-model은 value로 양방향 binding해주는 옵션 >> value로 props, emit하는 방법 -->
-      <button>Submit</button>
-    </form>
-    실시간으로 자식 컴포넌트에서 <br>값을 올려받는 부모의 name : <br>{{ name }}
+    <InputField v-model="name" /> <!-- v-model은 value로 양방향 binding해주는 옵션 >> value로 props, emit하는 방법 -->
+    <button @click="updateName">Submit</button>
+    {{ name }}
   </div>
 </template>
 
@@ -18,9 +15,38 @@ export default { // data, methods, computed ....
   },
   data() {
     return {
-      name: ''
+      name: 'Kossie Coder'
     }
   },
+  beforeCreate() {
+    console.log('beforeCreate',this.name) // data 접근 불가
+  },
+  created() {
+    console.log('created',this.name) // data 접근 가능
+  },
+  beforeMount() {
+    alert('beforeMount')
+  },
+  mounted() {
+    alert('mounted') // #app에 마운트
+  },
+  beforeUpdate() { // event 시작 부분 
+    alert('beforeUpdate')
+  },
+  updated() {
+    alert('updated')
+  },
+  beforeDestroy() {
+    alert('beforeDestroy')
+  },
+  destroyed() {
+    alert('destroyed')
+  },
+  methods: {
+    updateName() { // form 태그 안에다 버튼을 눌러서 함수를 바인딩시키면 함수 동작 안됨 
+      this.name = 'hello~~';
+    }
+  }
   // methods: {
   //   updateName(name) {
   //     this.name = name; // 자식에서 올려받은 것을 넣어줌
